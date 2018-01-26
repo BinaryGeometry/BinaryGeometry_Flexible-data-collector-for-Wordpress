291de8d37ad18aba4e2c0574f8d443e32ab27cc7
BinaryGeometry <binarygeometry@gmail.com>
2018-01-26 04:51:12 +0000

    tidying up

--- application/frontend/snowbotica-data-collector.js
+++ application/frontend/snowbotica-data-collector.js
@@ -2,128 +2,212 @@
 var snwbDatacollectorAjaxNonce = window.snwb_datacollector_api_object.ajax_nonce;
 var snwbDatacollectorAjaxUrl = window.snwb_datacollector_api_object.ajax_url;
 
+ 	// FormValidator.prototype.attributeValue = function (element, attributeName) {
+  //       var i;
+
+  //       if ((element.length > 0) && (element[0].type === 'radio' || element[0].type === 'checkbox')) {
+  //           for (i = 0, elementLength = element.length; i < elementLength; i++) {
+  //               if (element[i].checked) {
+  //                   return element[i][attributeName];
+  //               }
+  //           }
+
+  //           return;
+  //       }
+
+  //       return element[attributeName];
+  //   };
+
+    /**
+     * private function _getValidDate: helper function to convert a string date to a Date object
+     * @param date (String) must be in format yyyy-mm-dd or use keyword: today
+     * @returns {Date} returns false if invalid
+     */
+    FormValidator.prototype.validateField = function(key) {
+    
+
+    //       	// if (this.fields.hasOwnProperty(key)) {
+    //             var field = this.fields[key] || {},
+    //                 element = this.form[field.name];
+
+    //             if (element && element !== undefined) {
+    //                 field.id = attributeValue(element, 'id');
+    //                 field.element = element;
+    //                 field.type = (element.length > 0) ? element[0].type : element.type;
+    //                 field.value = attributeValue(element, 'value');
+    //                 field.checked = attributeValue(element, 'checked');
+
+                    
+    //                  * Run through the rules for each field.
+    //                  * If the field has a depends conditional, only validate the field
+    //                  * if it passes the custom function
+                     
+
+    //                 if (field.depends && typeof field.depends === "function") {
+    //                     if (field.depends.call(this, field)) {
+    //                         this._validateField(field);
+    //                     }
+    //                 } else if (field.depends && typeof field.depends === "string" && this.conditionals[field.depends]) {
+    //                     if (this.conditionals[field.depends].call(this,field)) {
+    //                         this._validateField(field);
+    //                     }
+    //                 } else {
+    //                     this._validateField(field);
+    //                 }
+    //             }
+    //         // }
+    	return;
+    }
+
+
 (function( $ ) {
  	var target = '.snwb-multipart-form form .wrapper';
  	// a useful object
  	var list = [];
  	var width = $(target).width();
- 	console.log(width);
- 	// $(target).css({
- 	// 	'min-width':width+'px'
- 	// });
- 	// $(target).append('<h1 style="width:100%">titld</h1>')
 
- 		$(target).find('.multipart-section').each(function(){
- 			var $formSection = $(this); // the current section
- 			var position = $formSection.data('position'); // the position of the current section
 
- 			// save position
- 			list.push({position:position, element: $(this)});
 
- 			// var next = $formSection.find('.snwb-next') //  
- 			// bind handlers to continue button
- 			$formSection.find('.snwb-next').on('click', function(e){
- 				e.preventDefault();
- 				var $target = $(this).closest('.multipart-section');
- 				
- 				var tabIndex = (Number( $(this).attr('tabIndex') ) + 1);
+	// var validator = new FormValidator('snwb_dataCollector_validate_this', [
+	// {
+	//     name: 'name',
+	//     display: 'required',
+	//     rules: 'required|double_barrel'
+	// } 
+	// ], function(errors, event) {
+	//     if (errors.length > 0) {
+	//         // Show the errors
+	//     }
+	// });
 
- 				$target.trigger('form:moveForward',[position, tabIndex]);
- 			});
- 			// bind handlers to back button
- 			$formSection.find('.snwb-back').on('click', function(e){
- 				e.preventDefault();
- 				var $target = $(this).closest('.multipart-section');
+	// validator.registerCallback('double_barrel', function(value) {
+	// 	var valid = /\s/;
+	// 	var value = value;
+	// 	var k = valid.test(value);
+	// 	alert(k);
+	//     if (k) {
+	//         return true;
+	//     }
 
- 				// find first element in this section and subtract one from tabindex
- 				var tabIndex = function(){
- 					// get the previous form section by subtracting from current position
- 					var $shortlist = $(target).find('.multipart-section[data-position="'+(position - 1)+'"]');
- 					// hone in on the first form element (this could be radio group, select, texarea or input)
- 					$shortlist = $shortlist.find('.form-element').first();
- 					// now we find the first actual form element
- 					var $item = $shortlist.find(':input').first() ;
-					var tabindex = $item.attr('tabIndex');
-					return tabindex;
-				}
- 				$target.trigger('form:moveBackward',[position, tabIndex()]);
- 			});
- 			// bind submit handlers to form
- 			$formSection.find('.snwb-submit').on('click', function(e){
- 				e.preventDefault();
- 				var $target = $(this).closest('.multipart-section');
- 				$target.trigger('form:submitForm');
- 			});
+	//     return false;
+	// })
+	// validator.setMessage('double_barrel', "Please use both names");
 
- 			// form:moveForward
- 			$formSection.on('form:moveForward', function(event, position, tabIndex){ 
- 				console.log('moving forward from: ', position, tabIndex)
- 				// move current/previous slide off left
- 				$(target).find('.multipart-section[data-position="'+position+'"]').css({
- 					left: -width+'px'
- 				});
- 				// move next slide to middle
- 				$(target).find('.multipart-section[data-position="'+(position + 1)+'"]').css({
- 					left: '0px'
- 				});	
- 				// focus on topmost form element
- 				// $(target).find('.multipart-section[data-position="'+(position + 1)+'"]')
- 				// .find(':input[tabindex='+tabIndex+']').focus();
+	// validator.validateField('name');
 
- 			});
 
- 			// form:moveBackward
- 			$formSection.on('form:moveBackward', function(event, position, tabIndex){ 
- 				console.log('moving backward from: ', position, tabIndex)
- 				// change to previous form section
- 				// move this off left
- 				$(target).find('.multipart-section[data-position="'+position+'"]').css({
- 					left:width+'px'
- 				});
- 				$(target).find('.multipart-section[data-position="'+(position - 1)+'"]').css({
- 					left:'0px'
+	$(target).find('.multipart-section').each(function(){
+		var $formSection = $(this); // the current section
+		var position = $formSection.data('position'); // the position of the current section
 
- 				});
- 				
- 				// focus on topmost form element
- 				$(target).find('.multipart-section[data-position="'+(position - 1)+'"]').show()
- 				.find(':input[tabindex='+tabIndex+']').focus();
- 			});
+		// save position
+		list.push({position:position, element: $(this)});
 
- 			// form:submitForm
- 			$formSection.on('form:submitForm', function(event){
- 				console.log('form submitting')
- 				var data = $("form").serialize();
- 				console.log(data, snwbDatacollectorAjaxUrl, snwbDatacollectorAjaxNonce)
- 				jQuery.ajax({
-                    type: 'POST',
-                    url: snwbDatacollectorAjaxUrl,
-                    data: {
-                    	action: 'snwb_datacollector_save_form', 
-                    	security: snwbDatacollectorAjaxNonce,
-                    	data: data
-                    },
-                    success: function (a) {
-                        console.log(a)
-                    }
-                });
- 			}); 
- 		
- 			// only show first section of form
- 			if( $formSection.data('position') != 1){
- 				// $formSection.hide()
- 				$formSection.css({
- 					left: width+'px'
- 				})
- 			} else {
- 				$formSection.css({
- 					left: '0px'
- 				})
- 			}
- 			
- 	
- 		})
- 		$('.dob-datepicker').Zebra_DatePicker();
+		// var next = $formSection.find('.snwb-next') //  
+		// bind handlers to continue button
+		$formSection.find('.snwb-next').on('click', function(e){
+			e.preventDefault();
+			var $target = $(this).closest('.multipart-section');
+			
+			var tabIndex = (Number( $(this).attr('tabIndex') ) + 1);
+
+			$target.trigger('form:moveForward',[position, tabIndex]);
+		});
+		// bind handlers to back button
+		$formSection.find('.snwb-back').on('click', function(e){
+			e.preventDefault();
+			var $target = $(this).closest('.multipart-section');
+
+			// find first element in this section and subtract one from tabindex
+			var tabIndex = function(){
+				// get the previous form section by subtracting from current position
+				var $shortlist = $(target).find('.multipart-section[data-position="'+(position - 1)+'"]');
+				// hone in on the first form element (this could be radio group, select, texarea or input)
+				$shortlist = $shortlist.find('.form-element').first();
+				// now we find the first actual form element
+				var $item = $shortlist.find(':input').first() ;
+			var tabindex = $item.attr('tabIndex');
+			return tabindex;
+		}
+			$target.trigger('form:moveBackward',[position, tabIndex()]);
+		});
+		// bind submit handlers to form
+		$formSection.find('.snwb-submit').on('click', function(e){
+			e.preventDefault();
+			var $target = $(this).closest('.multipart-section');
+			$target.trigger('form:submitForm');
+		});
+
+		// form:moveForward
+		$formSection.on('form:moveForward', function(event, position, tabIndex){ 
+			console.log('moving forward from: ', position, tabIndex)
+			// move current/previous slide off left
+			$(target).find('.multipart-section[data-position="'+position+'"]').css({
+				left: -width+'px'
+			});
+			// move next slide to middle
+			$(target).find('.multipart-section[data-position="'+(position + 1)+'"]').css({
+				left: '0px'
+			});	
+			// focus on topmost form element
+			// $(target).find('.multipart-section[data-position="'+(position + 1)+'"]')
+			// .find(':input[tabindex='+tabIndex+']').focus();
+
+		});
+
+		// form:moveBackward
+		$formSection.on('form:moveBackward', function(event, position, tabIndex){ 
+			console.log('moving backward from: ', position, tabIndex)
+			// change to previous form section
+			// move this off left
+			$(target).find('.multipart-section[data-position="'+position+'"]').css({
+				left:width+'px'
+			});
+			$(target).find('.multipart-section[data-position="'+(position - 1)+'"]').css({
+				left:'0px'
+
+			});
+			
+			// focus on topmost form element
+			$(target).find('.multipart-section[data-position="'+(position - 1)+'"]').show()
+			.find(':input[tabindex='+tabIndex+']').focus();
+		});
+
+		// form:submitForm
+		$formSection.on('form:submitForm', function(event){
+			console.log('form submitting')
+			var data = $("form").serialize();
+			console.log(data, snwbDatacollectorAjaxUrl, snwbDatacollectorAjaxNonce)
+			jQuery.ajax({
+            type: 'POST',
+            url: snwbDatacollectorAjaxUrl,
+            data: {
+            	action: 'snwb_datacollector_save_form', 
+            	security: snwbDatacollectorAjaxNonce,
+            	data: data
+            },
+            success: function (a) {
+                console.log(a)
+            }
+        });
+		}); 
+
+		// only show first section of form
+		if( $formSection.data('position') != 1){
+			// $formSection.hide()
+			$formSection.css({
+				left: width+'px'
+			})
+		} else {
+			$formSection.css({
+				left: '0px'
+			})
+		}
+		
+
+	})
+	/* Doing job good job */
+	// $('.dob-datepicker').Zebra_DatePicker();
 
 
  		
